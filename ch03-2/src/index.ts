a43a54551f25f786e80f6308189eceb462fa2de5
dev-chloe <chloekim.4969@gmail.com>
2021-02-24 23:17:58 +0900

    인터페이스 선택 속성

--- ch03-2/src/index.ts
+++ ch03-2/src/index.ts
@@ -1,6 +1,7 @@
 interface Iperson {
   name: string
   age: number
+  etc?: boolean // 선택속성
 }
 let good: Iperson = { name: 'Jack', age: 22 }
 
@@ -13,5 +14,8 @@ let good: Iperson = { name: 'Jack', age: 22 }
 // name, age 속성이 비어서 오류
 // let bad3: Iperson = {} 
 
-// etc 속성이 있어서 오류
+// etc 속성이 있어서 오류 (선택 속성이 없을 경우)
 // let bad4: Iperson = { name: 'Jack', age: 22, etc: true } 
+
+// etc 속성이 있어서 오류 (선택 속성이 있을 경우)
+let good2: Iperson = { name: 'Jack', age: 22, etc: true } 
