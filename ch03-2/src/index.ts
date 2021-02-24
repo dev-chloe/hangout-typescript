e2b06ddd39a2625fd4d2b8dc0bd40571ce639dc4
dev-chloe <chloekim.4969@gmail.com>
2021-02-24 23:16:08 +0900

    인터페이스 조건을 벗어나는 예

---
+++ ch03-2/src/index.ts
@@ -0,0 +1,17 @@
+interface Iperson {
+  name: string
+  age: number
+}
+let good: Iperson = { name: 'Jack', age: 22 }
+
+// age 속성이 비어서 오류
+// let bad1: Iperson = { name: 'koko' } 
+
+// name 속성이 비어서 오류
+// let bad2: Iperson = { age: 2 } 
+
+// name, age 속성이 비어서 오류
+// let bad3: Iperson = {} 
+
+// etc 속성이 있어서 오류
+// let bad4: Iperson = { name: 'Jack', age: 22, etc: true } 
---
+++ ch03-2/src/person/IPerson.ts
@@ -0,0 +1,5 @@
+
+export default interface Iperson {
+  name: string,
+  age: number
+}
